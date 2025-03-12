7f664a43856f8f88129ad5c7b31bdc328d57fb94
Tobias Bollinger <tobias.bollinger@gmail.com>
2025-03-12 21:44:52 +0100

    refactor booking scripts to use argparse for command-line parameters and improve function signatures

--- kirby-cafe-reservation.py
+++ kirby-cafe-reservation.py
@@ -1,49 +1,63 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import Select
 from selenium.webdriver.edge.options import Options
 from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.common.action_chains import ActionChains
+import argparse
 
-def create_booking(day_of_month: int, month: int, num_of_guests: int):
 
+def create_booking(num_of_guests: int):
+    '''Create a reservation for Kirby Cafe in Tokyo
+    Keyword arguments:
+    month -- month to book
+    num_of_guests -- number of guests to book
+    '''
     website = "https://kirbycafe-reserve.com/guest/tokyo/"
-
     edge_options = Options()
     edge_options.add_experimental_option("detach", True)
     driver = webdriver.Edge(options=edge_options)
-
     driver.get(website)
 
     try:
         driver.implicitly_wait(10)
-        driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+        driver.execute_script(
+            "window.scrollTo(0, document.body.scrollHeight);")
 
-        # agree to terms
-        driver.find_element(By.XPATH, "//label[contains(text(), '上記の内容を確認しました')]").click()
+        # Agree to terms
+        driver.find_element(
+            By.XPATH, "//label[contains(text(), '上記の内容を確認しました')]").click()
 
-        # click on next
+        # Click on next
         driver.find_element(By.CSS_SELECTOR, "a.v-btn").click()
 
-        # popup click on ok
-        driver.find_element(By.CSS_SELECTOR, ".v-card__actions button.v-btn").click()
-        
-        # number of guests
-        driver.find_element(By.CSS_SELECTOR, ".reserve-card .layout div.v-input").click()
-        driver.find_element(By.XPATH, "//div[contains(@id, '"+ str(num_of_guests) +"')]").click()
+        # Popup click on ok
+        driver.find_element(
+            By.CSS_SELECTOR, ".v-card__actions button.v-btn").click()
 
-        # select next month
-        driver.find_element(By.XPATH, "//button//span[contains(text(), '" + str(month) + "月')]").click()
+        # Number of guests
+        driver.find_element(
+            By.CSS_SELECTOR, ".reserve-card .layout div.v-input").click()
 
-        # chnage language
-        driver.find_element(By.XPATH, "//button//span//span[contains(text(), 'JP')]").click()
-        driver.find_element(By.XPATH, "//div//div[contains(text(), 'Eng')]").click()
+        guest_option_xpath = f"//div[contains(@id, '{num_of_guests}名様-list-item')]"
+        driver.find_element(By.XPATH, guest_option_xpath).click()
 
     except NoSuchElementException as e:
         print(e)
-        
-num_iterations = 2
-next_month=3
-day_of_month='10'
-num_of_guests=2
-[create_booking(day_of_month, next_month, num_of_guests) for x in range(num_iterations)]
+
+
+if __name__ == "__main__":
+    # Default parameters
+    num_of_guests = 2
+    iterations = 1
+
+    parser = argparse.ArgumentParser(
+        description='Create a reservation for Kirby Cafe.')
+    parser.add_argument('--num_of_guests', type=int,
+                        help='Number of guests to book', default=num_of_guests, required=False)
+    parser.add_argument('--iterations', type=int,
+                        help='Number of iterations to run the booking', default=iterations, required=False)
+
+    args = parser.parse_args()
+
+    for _ in range(args.iterations):
+        create_booking(args.num_of_guests)
--- pokemon-cafe-reservation.py
+++ pokemon-cafe-reservation.py
@@ -5,19 +5,22 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.edge.options import Options
 from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.common.action_chains import ActionChains
+import argparse
 
 
-def create_booking(day_of_month, num_of_guests, location):
-    '''Create a reservation for Pokemon Cafe in Tokyo
+def create_booking(day_of_month: int, num_of_guests: int, location: str):
+    '''Create a reservation for Pokemon Cafe in Tokyo or Osaka
     Keyword arguments:
     day_of_month -- day of the month to book
     num_of_guests -- number of guests to book (1-8)
+    location -- location of the cafe ('Tokyo' or 'Osaka')
     '''
-
     if location == "Tokyo":
         website = "https://reserve.pokemon-cafe.jp/"
     elif location == "Osaka":
         website = "https://osaka.pokemon-cafe.jp/"
+    else:
+        raise ValueError("Invalid location. Choose 'Tokyo' or 'Osaka'.")
 
     edge_options = Options()
     edge_options.add_experimental_option("detach", True)
@@ -30,33 +33,33 @@ def create_booking(day_of_month, num_of_guests, location):
         driver.execute_script(
             "window.scrollTo(0, document.body.scrollHeight);")
 
-        # 席の予約 HTML 2 - Agree T&C
-        # driver.implicitly_wait(10)
+        # Agree to terms and conditions
         agreeCheck = driver.find_element(By.CSS_SELECTOR, "label.agreeChecked")
         ActionChains(driver).move_to_element(
             agreeCheck).click(agreeCheck).perform()
 
+        # Click on the next button
         driver.find_element(By.XPATH, "//*[@class='button']").click()
-
-        # side with captcha: https://reserve.pokemon-cafe.jp/reserve/auth_confirm
 
         # Wait for user to solve CAPTCHA
         print("Please solve the CAPTCHA...")
         WebDriverWait(driver, 300).until(
-            EC.presence_of_element_located((By.XPATH, "//a[@class='button arrow-down' and @href='/reserve/step1']"))
+            EC.presence_of_element_located(
+                (By.XPATH, "//a[@class='button arrow-down' and @href='/reserve/step1']"))
         )
 
-        driver.find_element(By.XPATH, "//a[@class='button arrow-down' and @href='/reserve/step1']").click()
+        # Proceed after CAPTCHA is solved
+        driver.find_element(
+            By.XPATH, "//a[@class='button arrow-down' and @href='/reserve/step1']").click()
 
-        # 席の予約 HTML 3 - Select number of guest
+        # Select number of guests
         select = Select(driver.find_element(By.NAME, 'guest'))
         select.select_by_index(num_of_guests)
 
-        # 席の予約 HTML 4 - Select from calendar
+        # Select the day from the calendar
         driver.find_element(
             By.XPATH, "//*[contains(text(), '次の月を見る')]").click()
         driver.implicitly_wait(10)
-        # driver.find_element(By.XPATH, "//*[contains(text(), " + str(day_of_month) + ")]").click()
         driver.find_element(
             By.XPATH, "//*[text()=" + str(day_of_month) + "]").click()
         driver.find_element(By.XPATH, "//*[@class='button']").click()
@@ -64,10 +67,27 @@ def create_booking(day_of_month, num_of_guests, location):
         print(e)
 
 
-num_iterations = 2
-day_of_month = '10'
-num_of_guests = 2
-location = 'Tokyo'
-# location = 'Osaka'
+if __name__ == "__main__":
 
-[create_booking(day_of_month, num_of_guests, location) for x in range(num_iterations)]
+    # Default values
+    num_iterations = 2
+    next_month = 3
+    day_of_month = '10'
+    num_of_guests = 2
+    location = 'Tokyo'
+
+    parser = argparse.ArgumentParser(
+        description='Create a reservation for Pokemon Cafe.')
+    parser.add_argument('--day_of_month', type=int,
+                        help='Day of the month to book', default=day_of_month, required=False)
+    parser.add_argument('--num_of_guests', type=int,
+                        help='Number of guests to book (1-8)', default=num_of_guests, required=False)
+    parser.add_argument('--location', type=str, choices=[
+                        'Tokyo', 'Osaka'], help='Location of the cafe (Tokyo or Osaka)', default=location, required=False)
+    parser.add_argument('--iterations', type=int,
+                        help='Number of iterations to run the booking', default=num_iterations, required=False)
+
+    args = parser.parse_args()
+
+    for _ in range(args.iterations):
+        create_booking(args.day_of_month, args.num_of_guests, args.location)
